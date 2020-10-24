b5e202596f8c05ae9ab1437c0f0b015b187e86fd
Abzed <ibraabzed@gmail.com>
2020-10-24 14:51:56 +0300

    remove gmail host

--- codeawards/settings.py
+++ codeawards/settings.py
@@ -28,13 +28,6 @@ MODE=config("MODE", default="dev")
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = config('G_MAIL')
-EMAIL_HOST_PASSWORD = config('G_PASSWORD')
-EMAIL_PORT = 587
-
 cloudinary.config( 
   cloud_name = config('CLOUD_NAME'), 
   api_key = config('CLOUD_API'),
